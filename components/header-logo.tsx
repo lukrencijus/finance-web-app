f95b4feda4fea05f1ca1de8eed6b3e1c625dcea3
Lukas <160043122+lukrencijus@users.noreply.github.com>
2026-04-02 23:27:59 +0300

    header, nav, layout

---
+++ components/header-logo.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export const HeaderLogo = () => {
+    return (
+        <Link href="/">
+            <div className='items-center hidden lg:flex'>
+                <Image src="/logo.svg" alt="Logo" width={28} height={28} />
+                <p className='text-lg font-bold text-white ml-2'>
+                    Money Tracker
+                </p>
+            </div>
+        </Link>
+    )
+}
